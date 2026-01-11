446b5129df82077e6ceb79638ad8fc2d2d903041
Daniel Allen <dan@aeternagroup.net>
2026-01-11 17:53:31 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "postgresql_flexible_server_virtual_endpoints" {
+  description = "All postgresql_flexible_server_virtual_endpoint resources"
+  value       = azurerm_postgresql_flexible_server_virtual_endpoint.postgresql_flexible_server_virtual_endpoints
+}
+
