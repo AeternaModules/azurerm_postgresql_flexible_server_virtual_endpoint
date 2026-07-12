2677c4a37bdd392aaecd63968772acc63d44add2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:43 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "postgresql_flexible_server_virtual_endpoints" {
-  description = "All postgresql_flexible_server_virtual_endpoint resources"
-  value       = azurerm_postgresql_flexible_server_virtual_endpoint.postgresql_flexible_server_virtual_endpoints
-}
 output "postgresql_flexible_server_virtual_endpoints_name" {
-  description = "List of name values across all postgresql_flexible_server_virtual_endpoints"
-  value       = [for k, v in azurerm_postgresql_flexible_server_virtual_endpoint.postgresql_flexible_server_virtual_endpoints : v.name]
+  description = "Map of name values across all postgresql_flexible_server_virtual_endpoints, keyed the same as var.postgresql_flexible_server_virtual_endpoints"
+  value       = { for k, v in azurerm_postgresql_flexible_server_virtual_endpoint.postgresql_flexible_server_virtual_endpoints : k => v.name }
 }
 output "postgresql_flexible_server_virtual_endpoints_replica_server_id" {
-  description = "List of replica_server_id values across all postgresql_flexible_server_virtual_endpoints"
-  value       = [for k, v in azurerm_postgresql_flexible_server_virtual_endpoint.postgresql_flexible_server_virtual_endpoints : v.replica_server_id]
+  description = "Map of replica_server_id values across all postgresql_flexible_server_virtual_endpoints, keyed the same as var.postgresql_flexible_server_virtual_endpoints"
+  value       = { for k, v in azurerm_postgresql_flexible_server_virtual_endpoint.postgresql_flexible_server_virtual_endpoints : k => v.replica_server_id }
 }
 output "postgresql_flexible_server_virtual_endpoints_source_server_id" {
-  description = "List of source_server_id values across all postgresql_flexible_server_virtual_endpoints"
-  value       = [for k, v in azurerm_postgresql_flexible_server_virtual_endpoint.postgresql_flexible_server_virtual_endpoints : v.source_server_id]
+  description = "Map of source_server_id values across all postgresql_flexible_server_virtual_endpoints, keyed the same as var.postgresql_flexible_server_virtual_endpoints"
+  value       = { for k, v in azurerm_postgresql_flexible_server_virtual_endpoint.postgresql_flexible_server_virtual_endpoints : k => v.source_server_id }
 }
 output "postgresql_flexible_server_virtual_endpoints_type" {
-  description = "List of type values across all postgresql_flexible_server_virtual_endpoints"
-  value       = [for k, v in azurerm_postgresql_flexible_server_virtual_endpoint.postgresql_flexible_server_virtual_endpoints : v.type]
+  description = "Map of type values across all postgresql_flexible_server_virtual_endpoints, keyed the same as var.postgresql_flexible_server_virtual_endpoints"
+  value       = { for k, v in azurerm_postgresql_flexible_server_virtual_endpoint.postgresql_flexible_server_virtual_endpoints : k => v.type }
 }
 
